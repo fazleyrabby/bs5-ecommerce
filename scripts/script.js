ca565c7d009710c466fa845cab0d02515f4e1fdc
fazleyrabby <fazley111@gmail.com>
2022-12-01 02:44:46 +0600

    cart, wishlist page

--- scripts/script.js
+++ scripts/script.js
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (window.scrollY > 50) {
           document.getElementById('navbar_top').classList.add('fixed-top');
           // add padding top to show content behind navbar
-          navbar_height = document.querySelector('header').offsetHeight;
+          let navbar_height = document.querySelector('header').offsetHeight;
           document.body.style.paddingTop = navbar_height + 'px';
         } else {
           document.getElementById('navbar_top').classList.remove('fixed-top');
