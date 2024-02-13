14c71a8058843e78438cad435e2cc05fe403818e
Md. Fazley Rabbi <fazley111@gmail.com>
2024-02-14 01:26:06 +0600

    vite config fix: added html pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,20 @@ const path = require('path')
 export default {
   root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, '/index.html'),
+        cart: path.resolve(__dirname, '/cart.html'),
+        wishlist: path.resolve(__dirname, '/wishlist.html'),
+        login: path.resolve(__dirname, '/login.html'),
+        register: path.resolve(__dirname, '/register.html'),
+        product: path.resolve(__dirname, '/product.html'),
+        products: path.resolve(__dirname, '/products.html'),
+        checkout: path.resolve(__dirname, '/checkout.html'),
+        // aboutjs: resolve(__dirname, 'about.js')
+      },
+    },
   },
   resolve: {
     alias: {
